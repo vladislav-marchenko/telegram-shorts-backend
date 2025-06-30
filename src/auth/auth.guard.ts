7f73de3408c1499f9f5bcfa6c44bc91d2975614f
Владислав <maksimsraud@gmail.com>
2025-06-30 14:18:22 +0300

    fix: auth guard body data

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest()
-    const initData = request.body
+    const initData = request.body.initData
 
     const isInitDataValid = isValid(initData, process.env.BOT_TOKEN)
     if (!isInitDataValid) {
