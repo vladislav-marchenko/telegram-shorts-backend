6847b45e229e57120a342e280e4bc338f1e0a805
Владислав <maksimsraud@gmail.com>
2025-07-07 19:59:59 +0300

    fix: added username lowercase

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -8,7 +8,7 @@ export class User {
   @Prop({ unique: true, immutable: true, min: 3, required: true })
   telegramId: string
 
-  @Prop({ unique: true, required: true })
+  @Prop({ unique: true, required: true, lowercase: true })
   username: string
 
   @Prop({ required: true })
